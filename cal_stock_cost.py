8787f304cc79cac15aeca1f93aea7d62fc33f134
zhxh <zhxhcoder@gmail.com>
2020-01-12 18:40:53 +0800

    限定价格时计算目标成交量

--- cal_stock_cost.py
+++ cal_stock_cost.py
@@ -5,14 +5,29 @@ transPrice = 6.08
 transAmount = 2100
 
 
-def get_commission(trans_price, trans_amount):
-    return max(5, trans_amount * trans_price * 0.0003)
-
-
+# 根据成交价与成交量计算交易成本价
 def cal_trans_cost(trans_price, trans_amount, current_cost, current_amount):
-    return (current_cost * current_amount + get_commission(trans_price, trans_amount) + trans_amount * trans_price) / (
-            current_amount + trans_amount)
+    return (current_cost * current_amount + max(5,
+                                                trans_amount * trans_price * 0.0003) + trans_amount * trans_price) / (
+                   current_amount + trans_amount)
 
 
 transCost = cal_trans_cost(transPrice, transAmount, currentCost, currentAmount)
 print("成交价格：" + transPrice.__str__() + " 成交量 " + transAmount.__str__() + " 成本价变为 " + transCost.__str__())
+
+# 根据目标成本价和成交价来计算成交量
+
+targetCost = 6.993345454545455
+
+
+# current_cost * current_amount + max(5,target_amount * trans_price * 0.0003) + target_amount * trans_price = target_cost*(current_amount + target_amount)
+# fee=max(5,target_amount * trans_price * 0.0003)
+# if target_amount * trans_price * 0.0003>=5
+
+def cal_target_amount(trans_price, target_cost, current_cost, current_amount):
+    return 0
+
+
+targetAmount = cal_target_amount(transPrice, targetCost, currentCost, currentAmount)
+
+print("成交价格：" + transPrice.__str__() + " 目标成本 " + transAmount.__str__() + "  " + transCost.__str__())
