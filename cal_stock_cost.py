ecde8a8b62fa1ea42d1420576068721cddb02ad6
zhxh <zhxhcoder@gmail.com>
2020-01-12 18:20:15 +0800

    计算交易后成本价

--- cal_stock_cost.py
+++ cal_stock_cost.py
@@ -2,17 +2,17 @@ currentCost = 7.556
 currentAmount = 3400
 
 transPrice = 6.08
-transAmount = 3400
+transAmount = 2100
 
 
-def getCommission(transPrice, transAmount):
-    return max(5, transAmount * transPrice * 0.0003)
+def get_commission(trans_price, trans_amount):
+    return max(5, trans_amount * trans_price * 0.0003)
 
 
-def calTransCost(transPrice, transAmount, currentCost, currentAmount):
-    return (currentCost * currentAmount + getCommission(transPrice, transAmount) + transAmount * transPrice) / (
-            currentAmount + transAmount)
+def cal_trans_cost(trans_price, trans_amount, current_cost, current_amount):
+    return (current_cost * current_amount + get_commission(trans_price, trans_amount) + trans_amount * trans_price) / (
+            current_amount + trans_amount)
 
 
-transCost = calTransCost(transPrice, transAmount, currentCost, currentAmount)
+transCost = cal_trans_cost(transPrice, transAmount, currentCost, currentAmount)
 print("成交价格：" + transPrice.__str__() + " 成交量 " + transAmount.__str__() + " 成本价变为 " + transCost.__str__())
