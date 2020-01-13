39c827a6a181ffc23915ee878833c760b3e53ece
zhxh <zhxhcoder@gmail.com>
2020-01-13 10:29:28 +0800

    限定价格时计算目标成交量取最大值

--- cal_stock_cost.py
+++ cal_stock_cost.py
@@ -1,8 +1,8 @@
-currentCost = 7.556
-currentAmount = 3400
+currentCost = 7.0
+currentAmount = 3500
 
-transPrice = 6.08
-transAmount = 2100
+transPrice = 5.00
+transAmount = 2500
 
 
 # 根据成交价与成交量计算交易成本价
@@ -15,9 +15,11 @@ def cal_trans_cost(trans_price, trans_amount, current_cost, current_amount):
 transCost = cal_trans_cost(transPrice, transAmount, currentCost, currentAmount)
 print("成交价格：" + transPrice.__str__() + " 成交量 " + transAmount.__str__() + " 成本价变为 " + transCost.__str__())
 
+
+
 # 根据目标成本价和成交价来计算成交量
 
-targetCost = 6.993345454545455
+targetCost = 6.1675
 
 
 # current_cost * current_amount + max(5,target_amount * trans_price * 0.0003) + target_amount * trans_price = target_cost*(current_amount + target_amount)
@@ -35,3 +37,5 @@ targetAmount = cal_target_amount(transPrice, targetCost, currentCost, currentAmo
 print(
     "成交价格：" + transPrice.__str__() + " 目标成本 " + targetCost.__str__() + " 目标交易量 " + targetAmount.__str__() + " 手续费 " + max(
         5, targetAmount * transPrice * 0.0003).__str__())
+
+
