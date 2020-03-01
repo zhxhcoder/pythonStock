459ea8bbd8a733f1508eb734d8ed4ca63cb31a4a
zhxh <zhxhcoder@gmail.com>
2020-03-01 12:33:40 +0800

    太阳花

--- tutle/tutle_clock.py
+++ tutle/tutle_clock.py
@@ -127,6 +127,7 @@ def main():
     Tick()
     turtle.mainloop()
 
+
 # 如果模块是被直接运行的，则代码块被运行，如果模块被import，则代码块不被运行。
 if __name__ == "__main__":
     main()
--- tutle/tutle_sun.py
+++ tutle/tutle_sun.py
@@ -3,10 +3,17 @@ import turtle
 turtle = turtle.Turtle()
 screen = turtle.getscreen()
 
-turtle.color('red', 'yellow')
-turtle.begin_fill()
-for i in range(50):
-    turtle.forward(200)
-    turtle.left(170)
-turtle.end_fill()
-screen.mainloop()
+
+def main():
+    turtle.color('red', 'yellow')
+    turtle.begin_fill()
+    for i in range(50):
+        turtle.forward(200)
+        turtle.left(170)
+    turtle.end_fill()
+    screen.mainloop()
+
+
+# 如果模块是被直接运行的，则代码块被运行，如果模块被import，则代码块不被运行。
+if __name__ == "__main__":
+    main()
